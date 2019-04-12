c7cd91da76159c88ac3250f08a02d138d8689bdf
Patrick McIntosh <mcintoshpt@gmail.com>
2019-04-12 11:13:11 -0500

    Second Commit

--- CrimeAnalyzer/Program.cs
+++ CrimeAnalyzer/Program.cs
@@ -71,7 +71,7 @@ namespace CrimeAnalyzer
                 try
                 {
                     using (StreamReader reader = new StreamReader(CrimeCSVFile))
-                    //using (var reader = new StreamReader(CrimeCSVFile))
+                    
                     {
                         int FileLines = 0;
 
@@ -89,7 +89,7 @@ namespace CrimeAnalyzer
                                 Console.WriteLine("There should be 11 columns");
                                 Environment.Exit(0);
                             }
-                            
+
                             try
                             {
                                 year = int.Parse(values[0]);
@@ -109,50 +109,6 @@ namespace CrimeAnalyzer
 
                                 crimeList.Add(crime);
 
-                                //Testing note... Its printing all the values in the column for each statistic, so that works
-                                //Console.WriteLine("crimeList value {0}", crime.Burglary);
-                                //Console.WriteLine("year value {0}", crime.Year);
-
-                                //the from Crime I think is one of main sources of the errors
-                                var startYear = (from Crime in crimeList select crime.Year).Min();
-                                var lastYear = (from Crime in crimeList select crime.Year).Max();
-                                var range = lastYear - startYear;
-
-                                Console.WriteLine("Crime Analyzer Report");
-                                Console.WriteLine("");
-                                Console.WriteLine("Crime Report Period: {0} to {1}", startYear, lastYear);
-                                Console.WriteLine("");
-                                Console.WriteLine("Total Crime Report Years: {0}", lastYear - startYear);
-
-                                var fifteenThousand = from Crime in crimeList where crime.Murder < 15000 select crime.Year;
-                                Console.WriteLine("Years murder is less than 15000: {0}", fifteenThousand);
-
-                                var fiveHThousand = from Crime in crimeList where crime.Robbery > 500000 select crime.Robbery;
-                                Console.WriteLine("Years where robberies were greater than 500000: {0}", fiveHThousand);
-
-                                var violentCrimeTwentyTen = from Crime in crimeList where crime.Year == 2010 select (crime.ViolentCrime / crime.Population);
-                                Console.WriteLine("Violent crime per capita rate for 2010: {0}", violentCrimeTwentyTen);
-
-                                var averageMurders = (from Crime in crimeList select crime.Murder).Average();
-                                Console.WriteLine("Average murder per year (all years): {0}", averageMurders);
-
-                                var avgMNineFourNineSeven = (from Crime in crimeList where crime.Year >= 1994 && crime.Year <= 1997 select crime.Murder).Average();
-                                Console.WriteLine("Average number of murders per year for 1994-1997: {0}", avgMNineFourNineSeven);
-
-                                var avgMTwentyTenTwentyThirteen = (from Crime in crimeList where crime.Year >= 2010 && crime.Year <= 2013 select crime.Murder).Average();
-                                Console.WriteLine("Average number of murders per year for 2010-2013: {0}", avgMTwentyTenTwentyThirteen);
-
-                                var minTheft = (from Crime in crimeList where crime.Year >= 1999 && crime.Year <= 2004 select crime.Theft).Min();
-                                Console.WriteLine("Minimum number of thefts per year for 1999-2004: {0}", minTheft);
-
-                                var maxTheft = (from Crime in crimeList where crime.Year >= 1999 && crime.Year <= 2004 select crime.Theft).Max();
-                                Console.WriteLine("Maximum number of thefts per year for 1999-2004: {0}", maxTheft);
-
-                                var highestMVTheft = (from Crime in crimeList where crime.Year >= 1994 && crime.Year <= 2013 select crime.MotorVehicleTheft).Max();
-                                Console.WriteLine("Year with highest number of motor vehicle thefts: {0}", highestMVTheft);
-
-                               // Report.Add(range, averageMurders, fifteenThousand, fiveHThousand, violentCrimeTwentyTen, avgMNineFourNineSeven, avgMTwentyTenTwentyThirteen,
-                                //    minTheft, maxTheft, highestMVTheft);
                             }
 
 
@@ -160,17 +116,122 @@ namespace CrimeAnalyzer
                             {
                                 Console.WriteLine("Exception: {0}", e);
                             }
-                            return;
+                            //return;
                         }
-                    }
 
-                    //TextWriter tw = new StreamWriter(ReportFilePath);
-                    //{
-                    //    foreach (String s in Report)
-                    //        tw.WriteLine(s);
+                        //Testing note... Its printing all the values in the column for each statistic, so that works
+                        //Console.WriteLine("crimeList value {0}", crime.Burglary);
+                        //Console.WriteLine("year value {0}", crime.Year);
 
-                    //    tw.Close();
-                    //}
+                        string report = "Crime Analyzer Report\n\n";
+
+                        if (crimeList.Count() < 1)
+                        {
+                            report += "No data is available.\n";
+
+                        }
+
+                        var startYear = (from crime in crimeList select crime.Year).Min();
+                        var lastYear = (from crime in crimeList select crime.Year).Max();
+                        var range = lastYear - startYear;
+                        report += $"Period: {startYear}-{lastYear} ({crimeList.Count} years)\n";
+
+                        //Console.WriteLine("Crime Analyzer Report");
+                        //Console.WriteLine("");
+                        //Console.WriteLine("Crime Report Period: {0} to {1}", startYear, lastYear); //+++
+                        //Console.WriteLine("");
+                        //Console.WriteLine("Total Crime Report Years: {0}", (lastYear - startYear)+1); //+++
+                        report += "Years murders per year < 15000: ";
+
+                        var fifteenThousand = from crime in crimeList where crime.Murder < 15000 select crime.Year; //---
+
+                        if (fifteenThousand.Count() > 0)
+                        {
+                            foreach (var years in fifteenThousand)
+                            {
+                                report += years + ",";
+                            }
+                            report.TrimEnd(',');
+                            report += "\n";
+                        }
+
+                        //Console.WriteLine("Years murder is less than 15000: {0}", fifteenThousand);
+
+                        //Console.WriteLine();
+                        report += "Robberies per year > 500000: ";
+                        var fiveHThousand = from crime in crimeList where crime.Robbery > 500000 select crime; //---
+                        if (fiveHThousand.Count() > 0)
+                        {
+                            foreach (var record in fiveHThousand)
+                            {
+                                report += record.Year + " = " + record.Robbery + ",";
+                            }
+                            report.TrimEnd(',');
+                            report += "\n";
+                        }
+                        else
+                        {
+                            report += "not available\n";
+                        }
+
+
+                        //Console.WriteLine("Years where robberies were greater than 500000: {0}", fiveHThousand);
+                        report += "Violent crime per capita rate (2010): ";
+                        var violentCrimeTwentyTen = from crime in crimeList where crime.Year == 2010 select ((double)(crime.ViolentCrime) / (double)(crime.Population)); //---
+
+                        if (violentCrimeTwentyTen.Count() > 0)
+                        {
+                            report += violentCrimeTwentyTen.First();
+                            report += "\n";
+                        }
+                        else
+                        {
+                            report += "not available\n";
+                        }
+                        //Console.WriteLine("Violent crime per capita rate for 2010: {0}", violentCrimeTwentyTen);
+
+                        var averageMurders = (from crime in crimeList select crime.Murder).Average(); //+++
+                        //Console.WriteLine("Average murder per year (all years): {0}", averageMurders);
+                        report += $"Average murder per year (all years): {averageMurders}\n";
+
+                        var avgMNineFourNineSeven = (from crime in crimeList where crime.Year >= 1994 && crime.Year <= 1997 select crime.Murder).Average(); //+++
+                        //Console.WriteLine("Average number of murders per year for 1994-1997: {0}", avgMNineFourNineSeven);
+                        report += $"Average murder per year (1994 - 1997): {avgMNineFourNineSeven}\n";
+
+                        var avgMTwentyTenTwentyThirteen = (from crime in crimeList where crime.Year >= 2010 && crime.Year <= 2013 select crime.Murder).Average(); //+++
+                        //Console.WriteLine("Average number of murders per year for 2010-2013: {0}", avgMTwentyTenTwentyThirteen);
+                        report += $"Average murder per year (2010 - 2014): {avgMTwentyTenTwentyThirteen}\n";
+
+                        var minTheft = (from crime in crimeList where crime.Year >= 1999 && crime.Year <= 2004 select crime.Theft).Min(); //+++
+                        //Console.WriteLine("Minimum number of thefts per year for 1999-2004: {0}", minTheft);
+                        report += $"Minimum thefts per year (1999 - 2004): {minTheft}\n";
+
+                        var maxTheft = (from crime in crimeList where crime.Year >= 1999 && crime.Year <= 2004 select crime.Theft).Max(); //+++
+                        //Console.WriteLine("Maximum number of thefts per year for 1999-2004: {0}", maxTheft);
+                        report += $"Maximum thefts per year (1999 - 2004): {maxTheft}\n";
+
+
+                        report += "Year of highest number of motor vehicle thefts: ";
+                        var yearMaxMotorVehicleThefts = from crime in crimeList where crime.MotorVehicleTheft == ((from stats in crimeList select crime.MotorVehicleTheft).Max()) select crime.Year;
+                        if (yearMaxMotorVehicleThefts.Count() > 0)
+                        {
+                            report += yearMaxMotorVehicleThefts.First();
+                            report += "\n";
+                        }
+                        else
+                        {
+                            report += "not available\n";
+                        }
+                        //var highestMVTheft = (from crime in crimeList where crime.Year >= 1994 && crime.Year <= 2013 select crime.MotorVehicleTheft).Max(); //---
+                        //Console.WriteLine("Year with highest number of motor vehicle thefts: {0}", highestMVTheft);
+
+                        //Report.Add(range, averageMurders, fifteenThousand, fiveHThousand, violentCrimeTwentyTen, avgMNineFourNineSeven, avgMTwentyTenTwentyThirteen,
+                        // minTheft, maxTheft, highestMVTheft);
+
+                        Console.WriteLine(report);
+                        Console.WriteLine("");
+
+                        File.WriteAllText(ReportFilePath, report);
 
                         //Notes on what needs to be answered from the data
                         //What is the range of years included in the data?
@@ -185,8 +246,8 @@ namespace CrimeAnalyzer
                         //What is the maximum number of thefts per year for 1999 to 2004 ?
                         //What year had the highest number of motor vehicle thefts ?
 
+                    }
                 }
-
                 catch (Exception e)
                 {
                     Console.WriteLine("The file could not be read:");
